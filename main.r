b1f5c0a511f98995a67f44b29abe28b595f98d05
lin sam <610411102@ems.ndhu.edu.tw>
2016-12-30 16:22:04 +0800

    Add files via upload

--- main.r
+++ main.r
@@ -15,12 +15,19 @@
 	library(glmnet)
 	library(dplyr)
 	library(moments)
+	library(mice)
+	library(e1071)
+
 
 	setwd("g:\\kaggle3")
 
 	main.train=fread("train.csv")
 	main.test=fread("test.csv")
 	store <- fread("store.csv")
+
+	# 修補 open=na
+	na.num = filter(main.test,is.na(Open))$Id
+	main.test$Open[na.num]=1
 
 	#新增類別變數
 	num = c(1:1115)
@@ -35,19 +42,13 @@
 	#刪除sales=0的data，因為這種情形不合理
 	main.train4 = filter(main.train3,Sales !=0 )
 
-
 	#data 分割
 	main.train.y=filter(main.train4,date >=16600)#48天要預測當作y
 	main.train.x=filter(main.train4,date < 16600)#48天前資料
-
 	
-	#main.train.x=filter(main.train,date >0)#實際 預測10
-	#main.train.x.2=filter(main.train,date >= 16600)#實際 預測10
 	#-------------------------------------------------------------
+	result3 = pred.fun(main.train.x,main.train.y,main.test3)
 	#-------------------------------------------------------------
-	result1 = pred.fun(main.train.x,main.train.y,main.test3)
-	#-------------------------------------------------------------
-	result3 = result1 %>% arrange(Id)
 
 	gc()
 	write.csv(result3,"pred1.csv",row.names = FALSE)
